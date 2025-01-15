5fa23d57330c780086577dd8c905c5beffe17020
yahyaw889 <118213852+yahyaw889@users.noreply.github.com>
2025-01-15 19:37:59 +0200

    edit order error

--- app/Models/Special.php
+++ app/Models/Special.php
@@ -73,12 +73,7 @@ class Special extends Model
         });
 
 
-        static::saving(function ($model) {
-            $special = $model->special;
-            if (empty(array_filter($special, fn ($item) => !empty($item['name']) || !empty($item['price']) || !empty($item['image']) || !empty($item['size']) || !empty($item['color']) || !empty($item['order_id']) ))) {
-                $model->special = [];
-            }
-        });
+
     }
 
 

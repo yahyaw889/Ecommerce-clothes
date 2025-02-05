3fd31d771014dfe1e278ca600cf2c98135517978
yahyaw889 <118213852+yahyaw889@users.noreply.github.com>
2025-02-05 03:43:57 +0200

    final v1

--- routes/web.php
+++ routes/web.php
@@ -5,24 +5,10 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 
 
-Route::get('/{any}', function () {
-    return file_get_contents(public_path('../../frontend/index.html'));
-})->where('any', '.*');
 
 Route::get('/invoice/{id}/show', [PdfController::class , 'downloadPdf'])->name('invoice_download')->middleware('auth');
 
 Route::get('/invoice/{id}', [PdfController::class , 'generatePDF'])->name('invoice')->middleware('auth');
 
-
-
-// Route::get('refresh', function () {
-//     Artisan::call('view:clear');
-//     Artisan::call('cache:clear');
-//     Artisan::call('config:clear');
-//     Artisan::call('filament:optimize-clear');
-//     Artisan::call('filament:optimize');
-
-//     return ;
-// });
 
 
